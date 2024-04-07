b09bbffa356e8957d0f78a3346f00ba6749f41a1
Narayan <laxminarayanreddy432@gmail.com>
2024-04-07 12:42:33 +0530

    Initial Commit

---
+++ Rethread/RethreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  RethreadApp.swift
+//  Rethread
+//
+//  Created by narayan on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RethreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
